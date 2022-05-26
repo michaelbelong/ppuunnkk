0e8d50170e1ec82b58bc1a1f14f456e4f06534b1
michaelbelong <michaelbelong@gmail.com>
2022-05-26 08:11:26 -0700

    Do I have to keep doing this every time? Seems the last update I didn't add the new files. Well, here they are.

---
+++ custom/constants.js
@@ -0,0 +1,19 @@
+
+
+export const NAV_LINKS  = [
+  {
+	key: 1,
+	text: '𝙼𝙰𝙽𝙸𝙵𝙴𝚂𝚃𝙾',
+	link: 'https://twitter.com/okpcnft',
+  },
+  {
+	key: 2,
+	text: '𝚃𝚆𝙸𝚃𝚃𝙴𝚁',
+	link: 'https://discord.gg/okpc',
+  },
+  {
+	key: 3,
+	text: '𝙳𝙸𝚂𝙲𝙾𝚁𝙳',
+	link: 'https://zora.co/collections/0x7183209867489E1047f3A7c23ea1Aed9c4E236E8',
+  }
+]
