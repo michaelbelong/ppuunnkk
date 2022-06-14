4f43a869eed7e5a9abaf203900a5d768c6ff53e6
michaelbelong <michaelbelong@gmail.com>
2022-06-14 12:11:17 -0700

    Update DCRD Link

--- src/index.js
+++ src/index.js
@@ -10,8 +10,7 @@ root.render(
   <StrictMode>
     <ColorModeScript />
     <App />
-  </StrictMode>
-);
+  </StrictMode>);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
